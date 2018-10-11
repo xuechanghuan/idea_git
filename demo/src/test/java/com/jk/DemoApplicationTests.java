b885f81dc816b580adb67372a9354022de21a574
xuech <1632917013@qq.com>
2018-10-11 16:05:53 +0800

    第2次提交

--- demo/src/test/java/com/jk/DemoApplicationTests.java
+++ demo/src/test/java/com/jk/DemoApplicationTests.java
@@ -12,9 +12,9 @@ public class DemoApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println("11111111");
-        System.out.println("11111111");
-        System.out.println("11111111");
-        System.out.println("11111111");
+        System.out.println("22222222");
+        System.out.println("33333333");
+        System.out.println("44444444");
     }
 
 }
