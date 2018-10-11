919f54c58ec40003870321142bf683f9993c50fa
xuech <1632917013@qq.com>
2018-10-11 16:09:45 +0800

    第3次提交解决冲突

--- demo/src/test/java/com/jk/DemoApplicationTests.java
+++ demo/src/test/java/com/jk/DemoApplicationTests.java
@@ -11,10 +11,12 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println("11111111");
+        System.out.println("77777777");
         System.out.println("22222222");
         System.out.println("33333333");
         System.out.println("44444444");
+        System.out.println("55555555");
+        System.out.println("66666666");
     }
 
 }
