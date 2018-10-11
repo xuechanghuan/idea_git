78f81eb8cf99f41470f0c88432d21775e4a99b01
xuech <1632917013@qq.com>
2018-10-11 16:25:56 +0800

    master第1次提交

--- demo/src/test/java/com/jk/DemoApplicationTests.java
+++ demo/src/test/java/com/jk/DemoApplicationTests.java
@@ -14,8 +14,9 @@ public class DemoApplicationTests {
         System.out.println("11111111");
         System.out.println("22222222");
         System.out.println("33333333");
-        System.out.println("55555555");
-        System.out.println("44444444");
+        System.out.println("77777777");
+        System.out.println("88888888");
+        System.out.println("99999999");
     }
 
 }
